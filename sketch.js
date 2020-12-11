3cbc9f09ede9fe99270d8b92f43234fd2fddb466
saiyamsingla <74312500+saiyamsingla@users.noreply.github.com>
2020-12-11 13:03:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,52 +1,68 @@
-var wall,bullet,thickness;
-var speed,weight;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
 
 function setup() {
-  createCanvas(1600,400);
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-  speed=random(223,321);
-  weight=random(30,52);
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-  thickness=random(22,83);
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-  bullet=createSprite(50, 200, 50, 5);
-  bullet.velocityX=speed;
-  bullet.shapeColor=color(255);
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
 
 
-  wall=createSprite(1200, 200,thickness,height/2);
-  wall.shapeColor=color(80,80,80);
+	engine = Engine.create();
+	world = engine.world;
 
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6,isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
   
 }
+
 
 function draw() {
-  background("black");  
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
 
-  if(hasCollided(bullet,wall)){
-
-    bullet.velocityX=0;
-    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
-
-    if(damage>10){
-      wall.shapeColor=color(0,255,0);
-    }
-
-    if(damage<10){
-      wall.shapeColor=color(255,0,0);
-    }
-  }
-
+  keyPressed();
   drawSprites();
+ 
 }
 
-function hasCollided(lbullet,lwall){
-  bulletRightEdge=lbullet.x+lbullet.width;
-  wallLeftEdge=lwall.x;
- if(bulletRightEdge>=wallLeftEdge) {
-    return true;
+function keyPressed() {
+ if (keyDown(DOWN_ARROW)) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+	Matter.Body.setStatic(packageBody,false);
+    
   }
-   return false;
-  
 }
+
+
+
